2444cd6fde8984f4a94d3bbcb206fad6788e6acb
manaadithya <ms3698@srmist.edu.in>
2026-05-10 06:31:49 +0530

    Remove unused ESLint configuration options

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -12,12 +12,4 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-  
+

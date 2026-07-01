9cff1b7d8fd0364481796916f45ec230521bee8f
manaadithya <ms3698@srmist.edu.in>
2026-07-01 07:06:53 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,25 +2,5 @@ require('dotenv').config();
 const app = require('./app');
 // const { sequelize } = require('./models');
 
-const PORT = process.env.PORT || 5000;
 
-async function startServer() {
-  try {
-    const db = require('./models');
-    await db.sequelize.authenticate();
-    console.log('Database connected!');
-
-    // Sync models (use { force: true } only for dev/reset)
-    await db.sequelize.sync({ alter: true });
-    // await db.sequelize.sync();
-    console.log('Database synced!');
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error('Unable to start server:', error);
-  }
-}
-
-startServer();
+    console.error('Unable to
